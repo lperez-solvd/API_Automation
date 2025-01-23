c9535b166df8fb042d0440167a43621e6fe8b550
lperez-solvd <lperez@solvd.com>
2025-01-23 16:02:22 -0300

    / GraphQL test 5

--- src/test/java/com/solvd/gorest/user/GraphQLTests.java
+++ src/test/java/com/solvd/gorest/user/GraphQLTests.java
@@ -2,6 +2,7 @@ package com.solvd.gorest.user;
 
 import com.solvd.gorest.User;
 import com.solvd.gorest.utils.HttpStatus;
+import freemarker.template.TemplateException;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -24,7 +25,7 @@ public class GraphQLTests {
 
     String randomMail = createRandomEmail();
 
-    @Test (priority = 1)
+    @Test(priority = 1)
     public void getAllUsersByPageTest() {
 
         String query = "{ \"query\": \"query Users { users { nodes { email gender id name status } } }\" }";
@@ -55,7 +56,7 @@ public class GraphQLTests {
 
     }
 
-    @Test (priority = 2)
+    @Test(priority = 2)
     public void createUserTest() throws Exception {
 
         String jsonRequest = replaceEmailPlaceholder("src/test/resources/com.solvd.gorest/json/post/CreateUserRequest.json", randomMail);
@@ -98,7 +99,30 @@ public class GraphQLTests {
         Assert.assertEquals(response.jsonPath().getInt("data.deleteUser.user.id"), createdUserId, "The deleted user id is not the expected");
     }
 
+    @Test(dependsOnMethods = "createUserTest")
+    public void updateUserUser() throws Exception {
+
+        String jsonRequest = replaceEmailPlaceholder("src/test/resources/com.solvd.gorest/json/patch/modifyUserReq.json", randomMail);
+        User userRequest = convertJsonToUser(jsonRequest);
+
+        String query = String.format("{ \"query\": \"mutation UpdateUser { updateUser(input: { id: %d name: \\\"%s\\\" email: \\\"%s\\\" gender: \\\"%s\\\" status: \\\"%s\\\" clientMutationId: \\\"123123123\\\" }) { clientMutationId user { email name status id gender } } }\" }",
+                createdUserId, userRequest.getName(), userRequest.getEmail(), userRequest.getGender(), userRequest.getStatus());
 
 
+        Response response = given()
+                .auth()
+                .oauth2(accessToken)
+                .contentType(ContentType.JSON)
+                .body(query)
+                .post("https://gorest.co.in/public/v2/graphql");
+
+        response.then().statusCode(HttpStatus.GRAPHQL_OK);
+
+        String jsonResponse = replaceEmailPlaceholder("src/test/resources/com.solvd.gorest/json/patch/modifyUserReq.json", randomMail);
+        User userResponse = convertJsonToUser(jsonResponse);
+
+        Assert.assertEquals(userResponse, userRequest, "The request and response doesn't match");
+
+    }
 
 }
