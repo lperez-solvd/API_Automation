a2c863438d37a9f6b1d7088db2b54638e5aab269
lperez-solvd <lperez@solvd.com>
2025-01-30 10:05:29 -0300

    / Create files to store queries / Create loadQuery method to avoid hardconding

--- src/test/java/com/solvd/gorest/user/service/GraphQLService.java
+++ src/test/java/com/solvd/gorest/user/service/GraphQLService.java
@@ -8,6 +8,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -21,39 +26,32 @@ public class GraphQLService {
     private final PlaceholdersUtils placeholderUtils = new PlaceholdersUtils();
 
     public Response getUsers() {
-        String query = "{ \"query\": \"query Users { users { nodes { email gender id name status } } }\" }";
+        String query = loadGraphQLQuery("getUsers.graphql");
         return sendRequest(query);
     }
 
     public Response getUserById(int id) {
-        String query = String.format("{ \"query\": \"{ user(id: %s) { id name email } }\" }", id);
+        String query = completeQueryWithPlaceholders(loadGraphQLQuery("getUserById.graphql"), id);
         return sendRequest(query);
     }
 
     public Response createUser(User user) throws Exception {
 
-        String query = String.format("{ \"query\": \"mutation CreateUser { createUser(input: { name: \\\"%s\\\" email: \\\"%s\\\" gender: \\\"%s\\\"  status: \\\"%s\\\"  clientMutationId: \\\"123123123\\\" }) { clientMutationId user { email name status id gender } } }\" }",
-                user.getName(),
-                user.getEmail(),
-                user.getGender(),
-                user.getStatus());
+        String query = completeQueryWithPlaceholders(loadGraphQLQuery("createUser.graphql"), user.getName(), user.getEmail(), user.getGender(), user.getStatus());
 
         return sendRequest(query);
     }
 
     public Response deleteUser(int userId) {
 
-        String query = String.format("{ \"query\": \"mutation DeleteUser { deleteUser(input: { id: %s }) { user {id} } }\" }", userId);
-
+        String query = completeQueryWithPlaceholders(loadGraphQLQuery("deleteUser.graphql"), userId);
         return sendRequest(query);
     }
 
     public Response updateUser(int createdUserId, User userRequest) {
 
-        String query = String.format("{ \"query\": \"mutation UpdateUser { updateUser(input: { id: %d name: \\\"%s\\\" email: \\\"%s\\\" gender: \\\"%s\\\" status: \\\"%s\\\" clientMutationId: \\\"123123123\\\" }) { clientMutationId user { email name status id gender } } }\" }",
-                createdUserId, userRequest.getName(), userRequest.getEmail(), userRequest.getGender(), userRequest.getStatus());
-
-        return sendRequest(query);
+        String nquery = completeQueryWithPlaceholders(loadGraphQLQuery("updateUser.graphql"), createdUserId, userRequest.getName(), userRequest.getEmail(), userRequest.getGender(), userRequest.getStatus());
+        return sendRequest(nquery);
     }
 
     protected Response sendRequest(String query) {
@@ -69,6 +67,54 @@ public class GraphQLService {
         response.then().statusCode(HttpStatus.GRAPHQL_OK);
         return response;
 
+    }
+
+    // Query creator
+
+    private String loadGraphQLQuery(String fileName) {
+        Path path = Paths.get("src/test/java/com/solvd/gorest/user/service/queries/" + fileName);
+        try {
+            // Read the plain GraphQL query from the file
+            String query = Files.readString(path, StandardCharsets.UTF_8).trim();
+
+            // Remove newlines and excessive spaces from the GraphQL query
+            query = query.replaceAll("\\s+", " ");  // Replace multiple spaces with a single space
+            query = query.replace("\n", " ");        // Replace newlines with a space
+            query = query.replace("\r", " ");        // Replace carriage returns with a space
+
+            // Wrap the query in the proper JSON format
+            return "{ \"query\": \"" + query.replace("\"", "\\\"") + "\" }";
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load query file: " + fileName, e);
+        }
+    }
+
+    private String completeQueryWithPlaceholders(String query, int intPlaceholder) {
+        try {
+            return String.format(query, intPlaceholder);
+        } catch (Exception e) {
+            System.out.println("The query doesn't have the expected placeholders");
+            throw new RuntimeException();
+        }
+
+    }
+
+    private String completeQueryWithPlaceholders(String query, String placeholder1, String placeholder2, String placeholder3, String placeholder4) {
+        try {
+            return String.format(query, placeholder1, placeholder2, placeholder3, placeholder4);
+        } catch (Exception e) {
+            System.out.println("The query doesn't have the expected placeholders");
+            throw new RuntimeException();
+        }
+    }
+
+    private String completeQueryWithPlaceholders(String query, int placeholder1, String placeholder2, String placeholder3, String placeholder4, String placeholder5) {
+        try {
+            return String.format(query, placeholder1, placeholder2, placeholder3, placeholder4, placeholder5);
+        } catch (Exception e) {
+            System.out.println("The query doesn't have the expected placeholders");
+            throw new RuntimeException();
+        }
     }
 
     // helpers
@@ -103,7 +149,7 @@ public class GraphQLService {
         return response.body().jsonPath().getObject("data.updateUser.user", User.class);
     }
 
-    public int getDeletedUserId (Response response) {
+    public int getDeletedUserId(Response response) {
         return response.jsonPath().getInt("data.deleteUser.user.id");
     }
 
